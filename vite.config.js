96a156310de31de65d337041c91aaac496fc4331
Stanislav Kovtun <sstanisslaw@gmail.com>
2025-05-16 00:30:16 +0300

    Add GitHub deploy cction

--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,12 @@
-import { defineConfig } from "vitest/config";
-import react from "@vitejs/plugin-react";
+import { defineConfig } from 'vitest/config';
+import react from '@vitejs/plugin-react';
 
-// https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
-  test: {
-    environment: "jsdom",
-    setupFiles: ["./vitest.setup.js"],
-    globals: true,
-  },
+	base: '/tic-tac-toe-ai/',
+	plugins: [react()],
+	test: {
+		environment: 'jsdom',
+		setupFiles: ['./vitest.setup.js'],
+		globals: true,
+	},
 });
